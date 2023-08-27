583fc75b5208c7efc0c409969b0422f81a62db56
Tarun Kumar Vella <vella.tarun@gmail.com>
2023-08-27 07:58:04 +0530

    Added app.js as a layout purpose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { App } from "./app";
+import { App } from "./layout/app";
 const rootNode = document.getElementById('react');
 
 const root = ReactDOM.createRoot(rootNode);
