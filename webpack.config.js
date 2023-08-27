9ca2f8001ba469e732f8a3b78b7e6fb9f7b78fea
Tarun Kumar Vella <vella.tarun@gmail.com>
2023-08-27 07:53:40 +0530

    * Fixed html doctype issue with webpack config * introduced babel-loader to load react * without script injection the react is bootstraped

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,         // Match both .js and .jsx files
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query:
+                {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
